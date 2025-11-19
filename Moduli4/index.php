b1391bd6f0bbe604555baf895c2efd1a1050c648
gertibarca <gertinovoberdaliu@gmail.com>
2025-11-19 19:04:09 +0100

    Moduli4

---
+++ Moduli4/index.php
@@ -0,0 +1,67 @@
+<?php
+// phpinfo();
+// $x=1.5;
+// echo gettype($x);
+
+// function helloWorld(){
+//     echo "Hello World";
+// }
+
+// helloWorld();
+
+// function sum($x=2,$y=4){
+//     echo $x+$y;
+// }
+
+// sum(4,6);
+
+// function maximum($x,$y){
+//     if($x,$y){
+//         return $x;
+//     }else{
+//         return $y;
+//     }
+// }
+
+// $a=10;
+// $b=6;
+
+// $test=maximum($a,$b);
+// echo "The maximum of $a and $b is $test";
+
+// $x=10;
+// function localVariable(){
+//     $y=5;
+// }
+// echo $x;
+
+
+// // echo $sports[0]
+
+// // for($i=0;$i<4;$i++){
+// //     echo $sports[$i]. "<br>";
+// // }
+
+// $sports=['Football','Basketball','Handball','Voleyball','Golf','Tennis'];
+// // array_push($sports, 'PingPong');
+// // array_pop($sports);
+// // array_unshift($sports, 'Tennis');
+// // array_shift($sports);
+
+// //output=array_slice($sports,2);
+// //output=array_slice($sports,2,2);
+// $myvalue=[1,2,3,4,5];
+// var_dump(array_sum($myvalue));
+// // var_dum($output);
+
+
+
+$a = [5, 10, 15];
+
+$sum = array_sum($a);
+$avg = $sum / count($a);
+
+echo $avg;
+
+?>
+
