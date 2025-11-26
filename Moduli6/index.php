1c56792760fa3cceaedf5433bd030d89a5d2c263
gertibarca <gertinovoberdaliu@gmail.com>
2025-11-26 19:01:32 +0100

    Moduli6

---
+++ Moduli6/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// $my_file=fopen("ds.txt","w+");
+
+// fclose($my_file);
+
+// $my_files=fopen("ds.txt","r");
+
+// $file=fopen("example.txt","r");
+
+// while(!feof($file)){
+//     echo fgets($file) . "<br>";
+// }
+
+
+//     fclose($file);
+
+// $my_file=fopen("ds.txt","w");
+
+// $my_text="Digital School\n";
+
+// fwrite($my_file,$my_text);
+
+
+$h=fopen("data.txt","w+");
+
+fwrite($h,"Text test 5");
+
+$handle=fopen('data.txt',"a+");
+fwrite($handle, "Add more lines to the file");
+fclose($handle);
+?>
